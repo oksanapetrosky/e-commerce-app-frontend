d3210833926beea87c2d296bc083cba8df4bbdc5
Oksana Petrosky <165575688+oksanapetrosky@users.noreply.github.com>
2025-01-26 08:28:56 -0500

    Update assets.js

--- assets/assets.js
+++ assets/assets.js
@@ -116,7 +116,7 @@ import support_img from "./support_img.png";
 import menu_icon from "./menu_icon.png";
 import about_img from "./about_img.png";
 import contact_img from "./contact_img.png";
-import razorpay_logo from "./razorpay_logo.png";
+// import razorpay_logo from "./razorpay_logo.png";
 import stripe_logo from "./stripe_logo.png";
 import cross_icon from "./cross_icon.png";
 
@@ -136,7 +136,7 @@ export const assets = {
   menu_icon,
   about_img,
   contact_img,
-  razorpay_logo,
+  // razorpay_logo,
   stripe_logo,
   cross_icon,
 };
